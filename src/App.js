5a3cc63e6e9c7fc74558aacd8d540e503eec0d47
Albert Pichler <albertpichler@gmx.net>
2022-05-23 15:44:21 +0200

    Hit ctrl-s to trigger prettier

--- src/App.js
+++ src/App.js
@@ -379,15 +379,15 @@ function App() {
           <section css={videoSectionStyles}>
             <div className="videoWrapper" css={videoWrapperStyles}>
               <iframe
-      sandbox
-      width="853"
-      height="480"
-      src='https://www.youtube.com/embed/rokGy0huYEA'
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Embedded youtube"
-    />
+                sandbox
+                width="853"
+                height="480"
+                src="https://www.youtube.com/embed/rokGy0huYEA"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+              />
             </div>
           </section>
           <section css={containerMarginStyles}>
